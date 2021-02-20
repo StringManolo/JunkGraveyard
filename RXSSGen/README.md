c67c7ea1bd96da34cef25a8e236b9b710efb18d4
stringmanolo <manuelvarelacaldas@gmail.com>
2021-02-20 22:46:19 +0100

    Some old repos

---
+++ RXSSGen/README.md
@@ -0,0 +1,3 @@
+# RXSSGen
+Generador de Exploits para explotar reflected XSS via POST. 
+Ejemplo en vivo: https://output.jsbin.com/zesimaqavi
---
+++ SM_view-source/script.js
@@ -0,0 +1,1 @@
+javascript:document.write("<xmp contenteditable=\"true\" style=\"width:95%;height:500px;overflow-y:scroll;padding:10px;background-color:black;color:yellow;align:center\">"+document.getElementsByTagName('html')[0].outerHTML+"</xmp>"+document.getElementsByTagName('html')[0].outerHTML);
